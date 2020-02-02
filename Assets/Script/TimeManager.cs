3df556538dc5d158cf742bd3e3c241dff31a3301
Claudio Branca <claudio.branca@marshmallow-games.com>
2020-02-02 11:03:00 +0100

    bbbbbbbb

--- Assets/Script/TimeManager.cs
+++ Assets/Script/TimeManager.cs
@@ -46,7 +46,7 @@ public class TimeManager : MonoBehaviour
             {
                 time -= Time.deltaTime;
                 text.text = ((int)time).ToString();
-                Debug.Log((int)time);
+                //Debug.Log((int)time);
                 if (time <= 0.0f)
                 {
                     start = false;
@@ -60,7 +60,8 @@ public class TimeManager : MonoBehaviour
 
             }
         }
-        else{
+        else
+        {
             startanim.SetTrigger("win");
         }
     }
