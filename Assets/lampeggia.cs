33dddbf26aece4709b7e6aa7a3df343333f9a9c2
Andrea Bozzi <andrea.bozzi@marshamallow-games.com>
2020-02-02 13:23:26 +0100

    intro

---
+++ Assets/lampeggia.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lampeggia : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(lol());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator lol(){
+        while(true){
+        yield return new WaitForSeconds(.35f);
+        this.gameObject.GetComponent<Text>().enabled=false;
+        yield return new WaitForSeconds(.35f);
+        this.gameObject.GetComponent<Text>().enabled=true;
+        }
+    } 
+}
